d0e485dae9f60ce338e7afb73c7095868ba09ea4
Neel Patel <neelpatel@smu.edu>
2024-08-27 12:37:33 -0500

    set up chart on budget-planner

---
+++ finance-dashboard/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'], // Ensure Tailwind scans your Angular project files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
