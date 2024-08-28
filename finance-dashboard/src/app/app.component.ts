0870abedd892e12e83305484d17465c6ef12d8e8
Neel Patel <neelpatel@smu.edu>
2024-08-27 23:58:50 -0500

    set up navbar

--- finance-dashboard/src/app/app.component.ts
+++ finance-dashboard/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'finance-dashboard';
+
+  constructor(private authService: AuthService) {}
+
+  isLoggedIn(): boolean {
+    return !!this.authService.getCurrentUser(); // Return true if a user is logged in, false otherwise
+  }
 }
