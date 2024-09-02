f4c9c38e357588b2181a463f1b97fa41e609af9e
Neel Patel <neelpatel@smu.edu>
2024-09-02 04:56:30 -0500

    we're closeee

--- finance-dashboard/src/environments/environment.prod.ts
+++ finance-dashboard/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://fin-env.eba-qwcff6hq.us-east-1.elasticbeanstalk.com/api/', // Adjust the URL to match your backend setup
+    apiUrl: 'https://api.apifinancedashboard.com/api/', // Adjust the URL to match your backend setup
   };
