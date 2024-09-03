5aa05866fc7fd9438aa628fa5b18f3b091e390d5
Neel Patel <neelpatel@smu.edu>
2024-09-03 12:19:42 -0500

    see if routes work without the auth guard

--- finance-dashboard/src/app/app.routes.ts
+++ finance-dashboard/src/app/app.routes.ts
@@ -4,14 +4,13 @@ import { RegisterComponent } from './components/register/register.component';
 import { BudgetPlannerComponent } from './components/budget-planner/budget-planner.component';
 import { ExpenseTrackerComponent } from './components/expense-tracker/expense-tracker.component';
 import { GoalTrackerComponent } from './components/goal-tracker/goal-tracker.component';
-import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'budget-planner', component: BudgetPlannerComponent, canActivate: [AuthGuard] },
-    { path: 'expense-tracker', component: ExpenseTrackerComponent, canActivate: [AuthGuard] },
-    { path: 'goal-tracker', component: GoalTrackerComponent, canActivate: [AuthGuard] },
+  { path: 'budget-planner', component: BudgetPlannerComponent },
+    { path: 'expense-tracker', component: ExpenseTrackerComponent },
+    { path: 'goal-tracker', component: GoalTrackerComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
