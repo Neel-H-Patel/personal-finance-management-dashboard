c6d6117650e12a37b8ed72667662776be1480cff
Neel Patel <neelpatel@smu.edu>
2024-09-03 12:42:07 -0500

    add navbar to each component

--- finance-dashboard/src/app/app.component.ts
+++ finance-dashboard/src/app/app.component.ts
@@ -2,29 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
-import { AuthService } from './services/auth.service';
 import { CsrfService } from './services/csrf.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'finance-dashboard';
-  isLoggedIn = false;
 
-  constructor(private authService: AuthService, private csrfService: CsrfService) {}
+  constructor( private csrfService: CsrfService) {}
 
   ngOnInit() {
     // Initialize CSRF token when the app loads
     this.csrfService.initCsrfToken();
-
-    // Subscribe to currentUser to update isLoggedIn dynamically
-    this.authService.currentUser.subscribe(user => {
-      this.isLoggedIn = !!user; // Set to true if user is logged in, false otherwise
     });
   }
 }
